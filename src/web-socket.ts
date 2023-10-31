8e38e200061ffe2df48732f3971338cf52d4927f
shihanmilinda7 <shihanmilinda7@gmail.com>
2023-10-31 13:42:44 +0530

    m

--- src/web-socket.ts
+++ src/web-socket.ts
@@ -4,8 +4,8 @@ if (process.env.NODE_ENV === "development") {
   // Code to run in development environment
   webSocket = io("http://localhost:5000");
 } else {
-  webSocket = io(process.env.SOCKET_URL);
-  // webSocket = io("https://socket.ceyinfo.cloud");
+  // webSocket = io(process.env.SOCKET_URL);
+  webSocket = io("https://socket.ceyinfo.cloud");
 
   // Code to run in production environmentds
 }
